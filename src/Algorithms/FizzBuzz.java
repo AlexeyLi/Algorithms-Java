631a357ecf756c7e58e6b50713dce3b98672f78e
Alexey Li <alexeyli@alexeys-mbp.lan>
2020-10-16 20:30:22 -0700

    Added comments for FizzBuzz

--- src/Algorithms/FizzBuzz.java
+++ src/Algorithms/FizzBuzz.java
@@ -3,6 +3,11 @@ package Algorithms;
 import java.util.ArrayList;
 import java.util.List;
 
+/* Write a program that outputs the string representation of numbers from 1 to n.
+   But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+   For numbers which are multiples of both three and five output “FizzBuzz”.
+ */
+
 public class FizzBuzz {
 
     public static List<String> fizzBuzz(int n) {
