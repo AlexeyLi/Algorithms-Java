284b220a3e5d5f9cbe8c4c42b361351d205fe822
Alexey Li <alexeyli.sdet@gmail.com>
2022-04-11 17:41:45 -0700

    Add twoSum hashMap solution and fix unit tests

--- src/Algorithms/TwoSum.java
+++ src/Algorithms/TwoSum.java
@@ -1,17 +1,46 @@
 package Algorithms;
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*  1. Input: What is the input parameter?
+    2. Output: What is the datatype of your answer that your function has to return?
+    3. What data structures and methods associated with it will be used?
+    4. Where are you going to store temporary data (if needed)?
+    5. Describe the model of your algorithm before implementing the solution in code.
+    6. Write unit tests for your solution - think about edge cases.
+    7. Runtime complexity?
+    8. Memory complexity?
+ */
+
+import java.util.HashMap;
+
 public class TwoSum {
 
     public int[] twoSumBruteForce(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
                 if (nums[i] + nums[j] == target) {
-                    int[] result = {nums[i], nums[j]};
+                    int[] result = {i, j};
                     return result;
                 }
             }
         }
         return null;
     }
+    // Time: O(n^2)
+    // Memory: O(1)
 
+    public int[] twoSumHashmap(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+    // Time: O(n)
+    // Memory: O(n)
 }
--- src/Tests/TwoSumTest.java
+++ src/Tests/TwoSumTest.java
@@ -11,10 +11,19 @@ public class TwoSumTest {
     void testTwoSumBruteForce() {
         int[] array = {6, 3, 5, 2, 1, 7};
         int target = 4;
-        int[] expectedResult = {3, 1};
+        int[] expectedResult = {1, 4};
         TwoSum twoSum = new TwoSum();
         int[] actualResult = twoSum.twoSumBruteForce(array, target);
         Assertions.assertArrayEquals(expectedResult, actualResult);
     }
 
+    @Test
+    void testTwoSumHashMap() {
+        int[] array = {6, 3, 5, 2, 1, 7};
+        int target = 4;
+        int[] expectedResult = {1, 4};
+        TwoSum twoSum = new TwoSum();
+        int[] actualResult = twoSum.twoSumHashmap(array, target);
+        Assertions.assertArrayEquals(expectedResult, actualResult);
+    }
 }
